0b39174ce25421f40d9442c11fc72f08fbfca1ca
MStackPro <naszymanasseh@gmail.com>
2024-07-08 04:27:19 +0100

    initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,14 +6,24 @@ import Cart from "./pages/cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Footer from "./components/footer/Footer";
 import LoginPopUp from "./components/loginPopUp/LoginPopUp";
+import { motion, useScroll, useSpring } from "framer-motion";
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
+
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
   return (
     <>
-      {showLogin ? <LoginPopUp setShowLogin = {setShowLogin} /> : <></>}
+      {showLogin ? <LoginPopUp setShowLogin={setShowLogin} /> : <></>}
       <div className="app">
-        <Navbar setShowLogin = {setShowLogin} />
+        <motion.div className="progress-bar" style={{ scaleX }} />
+        
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
