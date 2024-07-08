4491792f6d892893455108e16a578288b688b0c3
MStackPro <naszymanasseh@gmail.com>
2024-07-08 02:06:17 +0100

    initial commit

---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredents and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+            <button>View Menu</button>
+        </div>
+    </header>
+  )
+}
+
+export default Header
